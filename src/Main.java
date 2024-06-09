01af221ad4dcadca8a612bebac6056ecc1a935ea
zhuang002 <zhuang001@gmail.com>
2024-06-09 11:09:33 -0400

    using ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		char[] songs = {'A','B','C','D','E'};
+		ArrayList<Character> songs = new ArrayList<>();
+		songs.add('A');
+		songs.add('B');
+		songs.add('C');
+		songs.add('D');
+		songs.add('E');
 		int op;
 		do {
 			op = sc.nextInt();
@@ -15,27 +21,20 @@ public class Main {
 		} while (op!=4);
 	}
 
-	private static void doOp(int op, char[] songs) {
+	private static void doOp(int op, ArrayList<Character> songs) {
 		// TODO Auto-generated method stub
 		if (op==1) {
-			char c = songs[0];
-			for (int i=0;i<songs.length-1;i++) {
-				songs[i]=songs[i+1];
-			}
-			songs[4] = c;
+			char c = songs.remove(0);
+			songs.add(c);
 		} else if (op==2) {
-			char c = songs[songs.length-1];
-			for (int i=songs.length-1;i>=1;i--) {
-				songs[i]=songs[i-1];
-			}
-			songs[0]=c;
+			char c = songs.remove(songs.size()-1);
+			songs.add(0,c);
 		} else if (op==3) {
-			char c = songs[0];
-			songs[0] = songs[1];
-			songs[1] = c;
+			char c = songs.remove(0);
+			songs.add(1,c);
 		} else if (op==4) {
-			for (int i=0;i<songs.length;i++) {
-				System.out.print(songs[i]+" ");
+			for (char c:songs) {
+				System.out.print(c+" ");
 			}
 		}
 	}
